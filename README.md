14b15d00c58ccd4ed8982886fc86ef4e0acb805d
Adriano27x <chatoadri27@gmail.com>
2026-05-14 01:02:13 -0500

    Agregue README profesional

---
+++ README.md
@@ -0,0 +1,41 @@
+# 🚀 Portfolio Web - Adriano
+
+## 👨‍💻 Desarrollador Web en formación
+
+Proyecto creado con HTML, CSS y JavaScript.
+
+Página moderna con:
+- 🎨 Diseño responsive
+- 🌙 Dark Mode
+- ✨ Animaciones
+- 📩 Formulario con EmailJS
+- 🐉 API de Pokémon
+- 💾 LocalStorage
+
+---
+
+# 🛠️ Tecnologías usadas
+
+- HTML5
+- CSS3
+- JavaScript
+- EmailJS
+- GitHub Pages
+
+---
+
+# 📸 Captura del proyecto
+
+![Preview](html/images/preview.jpg)
+
+---
+
+# 🌐 Demo del proyecto
+
+https://adriano27x.github.io/aprendizaje-web/
+
+---
+
+# 📌 Autor
+
+Adriano
--- javascript/script.js
+++ javascript/script.js
@@ -357,3 +357,82 @@ const observer = new IntersectionObserver(function(entries) {
 hiddenElements.forEach(function(element) {
     observer.observe(element);
 })
+
+//EMAILJS
+emailjs.init("f4NFOC4mDbTZHeImn");
+
+const formulario = document.getElementById("formularioContacto");
+const estadoMensaje = document.getElementById("estadoMensaje");
+const botonEnviar = formulario.querySelector("button");
+
+formulario.addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    const nombre = formulario.name.value.trim();
+    const correo = formulario.email.value.trim();
+    const mensajeTexto = formulario.message.value.trim();
+
+    // Validar campos vacíos
+    if (nombre == "" || correo == "" || mensajeTexto == "") {
+        estadoMensaje.textContent= "⚠️ Completa todos los campos";
+        estadoMensaje.className = "error";
+
+        return;
+    }
+
+    // Validar correo
+    if (!correo.includes("@") || !correo.includes(".")) {
+        estadoMensaje.textContent = "⚠️ Escribe un correo válido";
+        estadoMensaje.className = "error";
+
+        return;
+    }
+
+    // Cambiar texto botón
+    botonEnviar.textContent = "Enviando..."
+    botonEnviar.disabled = true;
+
+    emailjs.sendForm(
+        "service_aacp27",
+        "template_vy1pa7h",
+        this
+    )
+
+    .then(() => {
+
+        // Mensaje bonito
+        estadoMensaje.textContent = "✅ Mensaje enviado correctamente";
+        estadoMensaje.className = "exito";
+
+        // Limpiar formulario
+        formulario.reset();
+
+        setTimeout(() => {
+            estadoMensaje.textContent = "";
+        }, 3000);
+        
+    })
+
+    .catch((error) => {
+
+        estadoMensaje.textContent = "❌ Error al enviar mensaje:"
+        estadoMensaje.className = "error";
+
+        console.log(error);
+
+        setTimeout(() => {
+            estadoMensaje.textContent = "";
+        }, 3000);
+
+    })
+
+    .finally(() => {
+
+        // Restaurar botón
+        botonEnviar.textContent = "Enviar mensaje";
+        botonEnviar.disabled = false;
+
+    });
+
+});
