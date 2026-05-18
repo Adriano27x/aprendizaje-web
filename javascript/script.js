d929497bb2cd0f33dbb37e6cc28f57c7e2061f55
Adriano27x <chatoadri27@gmail.com>
2026-05-18 02:03:08 -0500

    Fix responsive navbar spacing

--- javascript/script.js
+++ javascript/script.js
@@ -560,11 +560,36 @@ menuToggle.addEventListener("click", function() {
 
 const navbar = document.querySelector("nav");
 
+let lastScroll = 0;
+
 window.addEventListener("scroll", function() {
 
+    // EFECTO SHADOW
     if (window.scrollY > 50) {
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
     }
+
+    // SOLO EN CELULAR/TABLET
+    if (window.innerWidth <= 768) {
+
+        let currentScroll = window.scrollY;
+
+        // Bajando
+        if (currentScroll > lastScroll && currentScroll > 80) {
+
+            navbar.style.transform = "translateY(-100%)";
+
+        }
+
+        // Subiendo
+        else {
+
+            navbar.style.transform = "translateY(0)";
+        }
+
+        lastScroll = currentScroll;
+    }
+
 });
